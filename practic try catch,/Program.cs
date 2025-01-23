585b02194f425302d19c81aa458c9adc7fa8fa1f
gokay <gokay@GOKAY>
2025-01-23 19:21:36 +0300

    Add project files.

---
+++ practic try catch,/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            try
+            {
+                // giriş alma
+                Console.Write("Lütfen bir sayı giriniz: ");
+                string girdi = Console.ReadLine();
+
+                // giriş sayıya dönüştürme
+                double sayi = Convert.ToDouble(girdi);
+
+                //sayının karesi
+                Console.WriteLine($"Girdiğiniz sayının karesi: {sayi * sayi}");
+                break;
+            }
+            catch (FormatException)
+            {
+                //geçersiz mesajı
+                Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+            }
+        }
+    }
+}
